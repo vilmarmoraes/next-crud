2a5bdc4d7e7cf45676558be93ce525c4b06de76c
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-05-13 14:13:39 -0300

    Adicionado componente Tabela e Icones

---
+++ src/components/Icones.tsx
@@ -0,0 +1,7 @@
+export const IconeEdicao = (
+    <div></div>
+)
+
+export const IconeLixo = (
+    <div></div>
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Layout from '../components/Layout'
-import styles from '../styles/Home.module.css'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/Cliente'
+
 
 export default function Home() {
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Bia', 45, '3'),
+    new Cliente('Bia', 45, '4'),
+  ]
+
+
   return (
     <div className={`
     flex justify-center items-center h-screen
-    bg-gradient-to-r from-blue-500 to-purple-500
+    bg-gradient-to-r from-blue-300 to-blue-50
     text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   )
