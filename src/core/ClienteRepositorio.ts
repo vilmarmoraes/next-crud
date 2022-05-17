f23a3911cc47e7ebed1a66301b973e6475c742d7
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-05-17 15:32:09 -0300

    Finalizado metodos de crud

--- src/core/ClienteRepositorio.ts
+++ src/core/ClienteRepositorio.ts
@@ -1,7 +1,10 @@
 import Cliente from "./Cliente";
 
 export default interface ClienteRepositorio {
+
     salvar(cliente: Cliente): Promise<Cliente>
+
     excluir(cliente: Cliente): Promise<void>
+
     obterTodos(): Promise<Cliente[]>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,72 +1,87 @@
-import { useState } from 'react'
-import Botao from '../components/Botao'
-import Formulario from '../components/Formulario'
-import Layout from '../components/Layout'
-import Tabela from '../components/Tabela'
-import Cliente from '../core/Cliente'
-
+import { useEffect, useState } from "react";
+import ColecaoCliente from "../backend/db/ColecaoCliente";
+import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
+import ClienteRepositorio from "../core/ClienteRepositorio";
 
 export default function Home() {
-  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const repo: ClienteRepositorio = new ColecaoCliente();
+
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio());
+  const [clientes, setClientes] = useState<Cliente[]>([]);
   //Controla alternancia entre tabela e formulário
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
-  
-  const clientes = [
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 45, '2'),
-    new Cliente('Carlos', 23, '3'),
-    new Cliente('Pedro', 54, '4'),
-  ]
+  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
 
-function clienteSelecionado(cliente: Cliente){
-  setCliente(cliente)
-  setVisivel('form')
-}
+  // const clientes = [
+  //   new Cliente('Ana', 34, '1'),
+  //   new Cliente('Bia', 45, '2'),
+  //   new Cliente('Carlos', 23, '3'),
+  //   new Cliente('Pedro', 54, '4'),
+  // ]
 
-function clienteExcluido(cliente: Cliente){
-  console.log(`Excluir ${cliente.nome}`)
-}
+  useEffect(obterTodos, []);
 
-function novoCliente() {
- setCliente(Cliente.vazio())
- setVisivel('form')
-}
-function salvarCliente(cliente: Cliente) {
- console.log(cliente)
- setVisivel('tabela')
-}
+  function obterTodos() {
+    repo.obterTodos().then((clientes) => {
+      setClientes(clientes);
+      setVisivel("tabela");
+    });
+  }
 
+  function clienteSelecionado(cliente: Cliente) {
+    setCliente(cliente);
+    setVisivel("form");
+  }
+
+  async function clienteExcluido(cliente: Cliente) {
+    await repo.excluir(cliente);
+    obterTodos();
+  }
+
+  function novoCliente() {
+    setCliente(Cliente.vazio());
+    setVisivel("form");
+  }
+
+  async function salvarCliente(cliente: Cliente) {
+    await repo.salvar(cliente);
+    obterTodos();
+  }
 
   return (
-    <div className={`
+    <div
+      className={`
       flex justify-center items-center h-screen
       bg-gradient-to-r from-blue-300 to-blue-50
       text-white
-    `}>
-      <Layout titulo="Cadastro Simples">        
-        {visivel === 'tabela' ? (
+    `}
+    >
+      <Layout titulo="Cadastro Simples">
+        {visivel === "tabela" ? (
           <>
             <div className="flex justify-end">
-              <Botao cor='green' classeName="mb-4" 
-                onClick={novoCliente}>
+              <Botao cor="green" classeName="mb-4" onClick={novoCliente}>
                 Novo Cliente
               </Botao>
             </div>
 
-            <Tabela clientes={clientes} 
+            <Tabela
+              clientes={clientes}
               clienteSelecionado={clienteSelecionado}
               clienteExcluido={clienteExcluido}
             />
           </>
         ) : (
-          <Formulario 
+          <Formulario
             cliente={cliente}
             clienteMudou={salvarCliente}
-            cancelado={() => setVisivel('tabela')}
-          />          
+            cancelado={() => setVisivel("tabela")}
+          />
         )}
-
       </Layout>
     </div>
-  )
+  );
 }
